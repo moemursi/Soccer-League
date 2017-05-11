b004963342d8d20a81637de8b2699976363065e8
Jimmy Smutek <jimmy@smutek.net>
2017-05-11 01:03:15 -0400

    Player Sorting
    Player sorting done. Need to sort as teams and output files next.

--- league_builder.py
+++ league_builder.py
@@ -10,36 +10,66 @@ def import_players(file, new_line, delimiter):
         return players
 
 
-# put logic here
+def sort_players(players):
+    # list to hold experienced players
+    inexperienced = []
+    # list to hold inexperienced players
+    experienced = []
+    # loop through players
+    for player in players:
+        # place in appropriate group
+        if player['Soccer Experience'] == "NO":
+            inexperienced.append(player)
+        else:
+            experienced.append(player)
+    # return groups
+    return inexperienced, experienced
+
+
+def assign_players(players, teams):
+    index = 0
+    # sort players into experienced and un-experienced
+    # returns 2 groups, inexperienced and experienced
+    sorted_players = sort_players(players)
+    # list of players to return to the script
+    player_list = []
+    # loop through the group
+    for group in sorted_players:
+        # set range = to the number of teams, use this
+        # value to select a team based on its current index
+        range = len(teams) - 1
+        # loop through players in this group
+        for player in group:
+            # assign player to the current team (index value)
+            player['Team'] = teams[index]
+            # add the updated player to a clean list
+            player_list.append(player)
+            # if index is less than the number of teams,
+            if index < range:
+                # increment by one..
+                index += 1
+            else:
+                # otherwise reset the index
+                index = 0
+    # return an updated list of players with their team assignments
+    return player_list
+
+
 if __name__ == "__main__":
     # open file & get players
-    players = import_players('soccer_players.csv', '', ',')
+    imported_players = import_players('soccer_players.csv', '', ',')
 
-    # setup teams
-    teams = {
+    # list of team names
+    team_names = [
         "Sharks",
         "Dragons",
         "Raptors"
-    }
+    ]
 
-    # sort players
-    inexperienced_players = []
-    experienced_players = []
+    player_list = assign_players(imported_players, team_names)
 
-    # sort into groups of experienced & un experienced
-    for player in players:
-        print(player['Name'], player['Soccer Experience'])
-
-        if player['Soccer Experience'] == "NO":
-            inexperienced_players.append(player)
-        else:
-            experienced_players.append(player)
-
-    for player in inexperienced_players:
-        print(player['Name'], player['Soccer Experience'])
-
-    for player in experienced_players:
-        print(player['Name'], player['Soccer Experience'])
+    for player in player_list:
+        print(player['Name'], player['Team'], player['Soccer Experience'])
 
 
 # save to teams
